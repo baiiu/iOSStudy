b17c44eaecbed3abb05025c0496422035ad01ed0
zhuzhe.zz <zhuzhe.zz@bytedance.com>
2025-03-25 15:03:01 +0800

    first apple app

--- iOSStudy/ViewController.m
+++ iOSStudy/ViewController.m
@@ -16,6 +16,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    UIButton * button = [UIButton buttonWithType:UIButtonTypeSystem];
+    button.frame=CGRectMake(40, 100, 240, 30);
+    button.backgroundColor=[UIColor redColor];
+    [button setTitle:@"点我一下"
+            forState:UIControlStateNormal];
+    [button addTarget:self
+            action:@selector(changeColor)
+            forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
+}
+
+
+// 按钮点击响应事件
+-(void)changeColor{
+    self.view.backgroundColor=[UIColor colorWithRed:arc4random()%255/255.0
+                                              green:arc4random()%255/255.0
+                                              blue:arc4random()%255/255.0
+                                              alpha:1];
 }
 
 
