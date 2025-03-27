32d09753f4b6d92c83be83e7e71edf9ad933f03a
zhuzhe.zz <zhuzhe.zz@bytedance.com>
2025-03-27 20:23:33 +0800

    add Person observeValueForKeyPath

--- iOSStudy/ViewController.m
+++ iOSStudy/ViewController.m
@@ -29,8 +29,19 @@
             action:@selector(changeColor)
             forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:button];
+    
+    
+    
+    UIButton * button2 = [UIButton buttonWithType:UIButtonTypeSystem];
+    button2.frame=CGRectMake(140, 200, 340, 130);
+    button2.backgroundColor=[UIColor redColor];
+    [button2 setTitle:@"点我一下"
+            forState:UIControlStateNormal];
+    [button2 addTarget:self
+            action:@selector(test)
+            forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button2];
 }
-
 
 // 按钮点击响应事件
 -(void)changeColor{
@@ -78,11 +89,40 @@
     
 }
 
+Person *p_global;
 
--(void)personDidSayHello{
-    NSLog(@"personDidSayHello");
+-(void) test {
+    Person *p2 = [[Person alloc]initWithNameAge:@"Tom":20];
+    p_global = p2;
+    [p_global addObserver:self forKeyPath:NSStringFromSelector(@selector(age)) options:NSKeyValueObservingOptionNew context:nil];
+    [p_global addObserver:self forKeyPath:NSStringFromSelector(@selector(name)) options:NSKeyValueObservingOptionNew context:nil];
+    
+    p2.name = @"Anna";
+    p2.age = 21;
+    [p2 updateAge:22];
 }
 
+
+-(void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context{
+    
+    if ([keyPath isEqualToString:NSStringFromSelector(@selector(age))] && object == p_global) {
+        NSNumber* number = [change objectForKey:NSKeyValueChangeNewKey];
+        int newAge = [[change objectForKey:NSKeyValueChangeNewKey]intValue];
+        NSLog(@"observeValueForKeyPath newAge: %d, %d", newAge, number);
+    }
+}
+
+
+-(void)dealloc {
+    [p_global removeObserver:self forKeyPath:NSStringFromSelector(@selector(age))];
+    [p_global removeObserver:self forKeyPath:NSStringFromSelector(@selector(name))];
+}
+
+
+#pragma mark PersonDelegate
+-(void)personDidSayHello {
+    NSLog(@"personDidSayHello");
+}
 
 
 @end
