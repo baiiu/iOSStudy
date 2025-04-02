7be5f7335d39b3e653b7340c99bf6621c8315506
zhuzhe.zz <zhuzhe.zz@bytedance.com>
2025-04-02 15:25:26 +0800

    add gcd

--- iOSStudy/ViewController.m
+++ iOSStudy/ViewController.m
@@ -26,21 +26,55 @@
     [button setTitle:@"点我一下"
             forState:UIControlStateNormal];
     [button addTarget:self
-            action:@selector(changeColor)
-            forControlEvents:UIControlEventTouchUpInside];
+               action:@selector(changeColor)
+     forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:button];
     
     
     
     UIButton * button2 = [UIButton buttonWithType:UIButtonTypeSystem];
-    button2.frame=CGRectMake(140, 200, 340, 130);
-    button2.backgroundColor=[UIColor redColor];
+    button2.frame=CGRectMake(40, 200, 340, 30);
+    button2.backgroundColor=[UIColor greenColor];
     [button2 setTitle:@"test other"
-            forState:UIControlStateNormal];
+             forState:UIControlStateNormal];
     [button2 addTarget:self
-            action:@selector(test)
-            forControlEvents:UIControlEventTouchUpInside];
+                action:@selector(test)
+      forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:button2];
+    
+    UIButton * button3 = [UIButton buttonWithType:UIButtonTypeSystem];
+    button3.frame=CGRectMake(40, 300, 340, 30);
+    button3.backgroundColor=[UIColor cyanColor];
+    [button3 setTitle:@"test_gcd"
+             forState:UIControlStateNormal];
+    [button3 addTarget:self
+                action:@selector(test_gcd)
+      forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button3];
+    
+    
+    
+    
+    // 创建并添加 RotatingView 组件到 ViewController 的主视图
+    UIView *rotatingView = [[UIView alloc] initWithFrame:CGRectMake(100, 700, 50, 50)];
+    rotatingView.backgroundColor = [UIColor blueColor];
+    [self.view addSubview:rotatingView];
+    [self addRotationAnimationToView:rotatingView];
+}
+
+// 创建并添加旋转动画到视图
+- (void)addRotationAnimationToView:(UIView *)view {
+    // 创建旋转动画
+    CABasicAnimation *rotationAnimation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
+    
+    // 设置动画属性
+    rotationAnimation.toValue = [NSNumber numberWithFloat:M_PI * 2.0]; // 旋转一周
+    rotationAnimation.duration = 2.0; // 持续时间
+    rotationAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear];
+    rotationAnimation.repeatCount = HUGE_VALF; // 无限重复
+    
+    // 将动画添加到视图的图层
+    [view.layer addAnimation:rotationAnimation forKey:@"rotationAnimation"];
 }
 
 // 按钮点击响应事件
@@ -118,17 +152,17 @@ Person *p_global;
     
     
     [NSNotificationCenter.defaultCenter
-        addObserver:self
+     addObserver:self
      selector:@selector(handleGreetingNotification:)
-        name:@"GreetingNotification"
-        object:nil];
+     name:@"GreetingNotification"
+     object:nil];
     
     NSNotification* noti = [[NSNotification alloc]initWithName:@"GreetingNotification" object:nil userInfo:nil];
     [NSNotificationCenter.defaultCenter postNotification:noti];
     [NSNotificationCenter.defaultCenter postNotificationName:@"GreetingNotification" object:nil];
     NSDictionary *userInfo = @{@"message": @"Hello, World!"};
     [[NSNotificationCenter defaultCenter] postNotificationName:@"GreetingNotification" object:nil userInfo:userInfo];
-
+    
     
     SEL selector1 = @selector(personDidSayHello);
     SEL selector2 = NSSelectorFromString(@"personDidSayHello");
@@ -177,17 +211,17 @@ Person *p_global;
     
     // 调用
     [invocation invoke];
-
-    NSLog(@"111");
-
-
     
-//    if ([self respondsToSelector:@selector(onAgeUpdated)]) {
-//        NSLog(@"ageUpdated found");
-//        [self performSelector:@selector(onAgeUpdated) withObject:1 withObject:2];
-//    } else {
-//        NSLog(@"ageUpdated not found");
-//    }
+    NSLog(@"111");
+    
+    
+    
+    //    if ([self respondsToSelector:@selector(onAgeUpdated)]) {
+    //        NSLog(@"ageUpdated found");
+    //        [self performSelector:@selector(onAgeUpdated) withObject:1 withObject:2];
+    //    } else {
+    //        NSLog(@"ageUpdated not found");
+    //    }
     
     
 }
@@ -218,6 +252,72 @@ Person *p_global;
     [NSNotificationCenter.defaultCenter removeObserver:self];
 }
 
+
+# pragma mark test_gcd
+
+- (void)printCurrentThreadID:(NSString*)logStr {
+    // 获取当前线程对象
+    NSThread *currentThread = [NSThread currentThread];
+    NSDate *currentDate = [NSDate date];
+
+    // 打印线程的标识符
+    NSLog(@"%@ Thread ID: %@, %@", currentDate, currentThread, logStr);
+}
+
+-(void) test_gcd {
+    [self printCurrentThreadID:@"test_gcd"];
+    
+    dispatch_queue_t main_queue = dispatch_get_main_queue();
+    dispatch_async(main_queue, ^{
+        [self printCurrentThreadID:@"111"];
+    });
+    
+//    [NSThread sleepForTimeInterval:5.0]; // 只会阻塞被调用线程，动画线程在core animation线程运行，不会阻塞
+    
+//    NSThread *thread = [[NSThread alloc]initWithBlock:^{
+//        @autoreleasepool {
+//            [self printCurrentThreadID:@"run in thread"];
+//            
+//            [NSThread sleepForTimeInterval:2.0];
+//            [self printCurrentThreadID:@"run in thread after 2s"];
+//            
+//            [self printCurrentThreadID:@"dispatch before"];
+//            dispatch_async(main_queue, ^{
+//                [self printCurrentThreadID:@"dispatch here111"];
+//                [NSThread sleepForTimeInterval:2.0];
+//                [self printCurrentThreadID:@"dispatch here111111"];
+//            });
+//            
+//            dispatch_sync(main_queue, ^{
+//                [self printCurrentThreadID:@"dispatch here222"];
+//                [NSThread sleepForTimeInterval:2.0];
+//                [self printCurrentThreadID:@"dispatch here222222"];
+//            });
+//            [self printCurrentThreadID:@"dispatch after"]; // 需要等待dispatch_sync完成
+//        }
+//    }];
+//    [thread start];
+    
+    
+    NSThread *thread2 = [[NSThread alloc]initWithBlock:^{
+        @autoreleasepool {
+            dispatch_queue_t main_queue = dispatch_get_main_queue();
+            for (int i = 0; i < 30; ++i) {
+                [self printCurrentThreadID:[NSString stringWithFormat:@"to dispatch block on none-main thread %d", i]];
+                // 因为是耗时任务，在main_queue中排队被执行
+                dispatch_async(main_queue, ^{
+                    [self printCurrentThreadID:[NSString stringWithFormat:@"test here %d", i]];
+                    [NSThread sleepForTimeInterval:2.0];
+                    [self printCurrentThreadID:[NSString stringWithFormat:@"after test here  %d", i]];
+                });
+            }
+        }
+    }];
+    [thread2 start];
+    
+    
+    
+}
 
 
 @end
